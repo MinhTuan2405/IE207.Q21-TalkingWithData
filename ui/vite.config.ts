bff1b3a51647f803c9a0670cf3afb7e48da8a1b1
mnhtndev <tuannguyen.02042005@gmail.com>
2026-01-11 00:01:55 +0700

    init: UI for the app

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+
+  // server config
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
+    strictPort: true
+  }
 })
