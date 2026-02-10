33817c49cb3e455a53e4c02415c9a93ea27539e1
mnhtndev <tuannguyen.02042005@gmail.com>
2026-02-10 12:11:29 +0700

    chore: re-structure the server component

--- server/main.py
+++ server/main.py
@@ -20,3 +20,9 @@ def root():
 @app.get("/health")
 def health():
     return {"status": "ok"}
+
+
+# Only use in the dev environment
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
