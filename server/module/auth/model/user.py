b91c07f8f3bcfacb8cbd5cfa35022693595c35f9
mnhtndev <tuannguyen.02042005@gmail.com>
2026-02-15 22:16:59 +0700

    add - server: fix shema for auth

--- server/module/auth/model/user.py
+++ server/module/auth/model/user.py
@@ -13,6 +13,5 @@ class User(Base):
     hashed_password = Column(String, nullable=False)
     full_name = Column(String, nullable=True)
     is_active = Column(Boolean, default=True)
-    is_superuser = Column(Boolean, default=False)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True), onupdate=func.now())
